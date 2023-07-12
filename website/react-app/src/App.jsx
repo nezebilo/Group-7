06230b8881a311ff12b0b9413b5affa30aded61f
CraneWvs <yiyun_uibe@126.com>
2023-07-12 04:30:22 +0100

    get data from /zones and fix the bug of duplicate axios requests

--- website/react-app/src/App.jsx
+++ website/react-app/src/App.jsx
@@ -10,34 +10,66 @@ import HeatmapPage from './components/HeatmapPage/HeatmapPage';
 function App() {
   return (
     <Router>
+      <HeaderContainer />
       <MyRoutes />
+      <Footer />
     </Router>
   );
 }
 
-function MyRoutes() {
+function HeaderContainer() {
   let location = useLocation();
 
+  return location.pathname !== '/login' && location.pathname !== '/signup' ? <Header /> : null;
+}
+
+function MyRoutes() {
   return (
     <div>
-      {/* Only show Header when not on LoginPage */}
-      {location.pathname !== '/login' && location.pathname !== '/signup' && <Header />}
-
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route path='/solutions' element={<HeatmapPage />} />
-
         <Route path="/login" element={<LoginPage />} />
-
         <Route path="/signup" element={<SignupPage/>}/>
       </Routes>
-      
-      {/* Only show Footer when not on LoginPage */}
-      {location.pathname !== '/login' && <Footer />}
     </div>
   );
 }
+
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <MyRoutes />
+//     </Router>
+//   );
+// }
+
+
+// function MyRoutes() {
+//   let location = useLocation();
+
+//   return (
+//     <div>
+//       {/* Only show Header when not on LoginPage */}
+//       {location.pathname !== '/login' && location.pathname !== '/signup' && <Header />}
+
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+
+//         <Route path='/solutions' element={<HeatmapPage />} />
+
+//         <Route path="/login" element={<LoginPage />} />
+
+//         <Route path="/signup" element={<SignupPage/>}/>
+//       </Routes>
+      
+//       {/* Only show Footer when not on LoginPage */}
+//       {location.pathname !== '/login' && <Footer />}
+//     </div>
+//   );
+// }
 // import './App.css';
 // import ZoneComponent from  './components/BackendAPI/ZoneComponent'
 // function App() {
--- website/react-app/src/components/HeatmapPage/HeatmapPage.jsx
+++ website/react-app/src/components/HeatmapPage/HeatmapPage.jsx
@@ -4,29 +4,46 @@ import 'leaflet/dist/leaflet.css';
 import "./HeatmapPage.css"
 import FunctionArea from './FunctionArea.jsx';
 import Map from './Map';
+import axios from 'axios';
 
 function Details({zone}) {
   console.log("zone in Details:", zone);
   return (
     <div className="DetailsCard">
       <h2>Zone Details</h2>
-      <p><strong>ID:</strong> {zone.properties.location_id}</p>
-      <p><strong>Zone:</strong> {zone.properties.zone}</p>
+      <p><strong>ID:</strong> {zone.id}</p>
+      <p><strong>Zone:</strong> {zone.properties.name}</p>
     </div>
   );
 }
 
 
 function HeatmapPage() {
+
+  console.log('Rendering HeatmapPage');
   const [zones, setZones] = useState(null);
   const [selectedZone, setSelectedZone] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log('Before fetching data');
+      // preflight requests? so twice?
+      const response = await axios.get("http://127.0.0.1:8000/api/zones/");
+      // const response = await axios.get("./NYC Taxi Zones.geojson");
+      console.log('After fetching data, before setting zones');
+      setZones(response.data);
+      console.log('After setting zones');
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="HeatmapPage">
       <FunctionArea />
       <div className="MapArea">
         <Map zones={zones} setZones={setZones} selectedZone={selectedZone} setSelectedZone={setSelectedZone} />
         <div className={selectedZone ? "DetailsContainer" : "DetailsContainer DetailsHidden"}>
-          {selectedZone && <Details zone={zones.features.find(zone => zone.properties.location_id === selectedZone)} />}
+          {selectedZone && <Details zone={zones.features.find(zone => zone.id === selectedZone)} />}
         </div>
       </div>
     </div>
@@ -35,3 +52,4 @@ function HeatmapPage() {
 
 
 export default HeatmapPage;
+// export default React.memo(HeatmapPage);
